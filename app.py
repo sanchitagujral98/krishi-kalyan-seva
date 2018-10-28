7f937a92fd8bfb4feea4593b997e42fc3f9ef575
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2018-10-28 08:47:37 +0530

    frontend modified

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ from main import recordAudio, bot
 app=Flask(__name__)
 app.config['DEBUG']=True
 
-@app.route('/voice_assistant', methods=['GET','POST'])
-def voice_assistant():
-    txt=recordAudio()
-    url='https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181027T233108Z.1dcc6673818e795c.6b2bd8112a5f748b44298d1f4ec7785926fad057&lang=en&text=+txt'
-    lang_data=[]
-    r = requests.get(url).json()
-    language={
-       'result': r['text'][0],
-    }
+@app.route('/vc', methods=['GET','POST'])
+def vc():
+    # txt=recordAudio()
+    # url='https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181027T233108Z.1dcc6673818e795c.6b2bd8112a5f748b44298d1f4ec7785926fad057&lang=en&text=+txt'
+    # lang_data=[]
+    # r = requests.get(url.format()).json()
+    # lang_data = r['text'][0]
+    # print(lang_data)
 
 
-    lang_data=lang_data.append(language)
-    en_data=lang_data[0]
-    output = bot(en_data)
+
+
+    #output = bot(lang_data)
 
     return render_template('index.html')
 
