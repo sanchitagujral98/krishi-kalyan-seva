0cb84bffcddcd8e45e2b4562c7c9ea019d02b1a7
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2018-10-28 07:36:03 +0530

    ml added

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app.config['DEBUG']=True
 
 @app.route('/voice_assistant', methods=['GET','POST'])
 def voice_assistant():
-    txt=data
+    txt=recordAudio()
     url='https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181027T233108Z.1dcc6673818e795c.6b2bd8112a5f748b44298d1f4ec7785926fad057&lang=en&text=+txt'
     lang_data=[]
-    r = requests.get(url.format(city.name)).json()
+    r = requests.get(url).json()
     language={
        'result': r['text'][0],
     }
@@ -18,7 +18,7 @@ def voice_assistant():
 
     lang_data=lang_data.append(language)
     en_data=lang_data[0]
-    
+    bot(en_data)
 
     return render_template('index.html',lang_data=lang_data,weather_data=weather_data)
 
